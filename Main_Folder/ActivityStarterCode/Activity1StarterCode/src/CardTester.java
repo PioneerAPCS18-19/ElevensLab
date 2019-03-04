b846f711a33026343ebde961099cf077aba55e3d
codytheking <codytheking93@gmail.com>
2019-03-04 12:15:27 -0800

    Act 1 starter code

---
+++ Main_Folder/ActivityStarterCode/Activity1StarterCode/src/CardTester.java
@@ -0,0 +1,21 @@
+
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+
+        // Create three Card objects, two of which are the same card
+
+        System.out.println(c1.matches(c2));
+		System.out.println(c2.matches(c3));
+		System.out.println(c1.matches(c3));
+		System.out.println(c1);
+	}
+}
