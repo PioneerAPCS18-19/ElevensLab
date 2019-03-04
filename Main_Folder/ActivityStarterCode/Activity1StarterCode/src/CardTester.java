8267c520daf1633e38e6f2d3c9ed9bb59c013457
codytheking <codytheking93@gmail.com>
2019-03-04 12:18:32 -0800

    Trying to fix formatting

--- Main_Folder/ActivityStarterCode/Activity1StarterCode/src/CardTester.java
+++ Main_Folder/ActivityStarterCode/Activity1StarterCode/src/CardTester.java
@@ -9,13 +9,13 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
 
         // Create three Card objects, two of which are the same card
 
         System.out.println(c1.matches(c2));
-		System.out.println(c2.matches(c3));
-		System.out.println(c1.matches(c3));
-		System.out.println(c1);
+        System.out.println(c2.matches(c3));
+        System.out.println(c1.matches(c3));
+        System.out.println(c1);
 	}
 }
