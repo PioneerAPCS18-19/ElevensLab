cec6ab9f8e97e7997f6a3aaf21e60b525ebd9131
codytheking <codytheking93@gmail.com>
2019-03-05 11:20:17 -0800

    Act 2 starter code

---
+++ Main_Folder/ActivityStarterCode/Activity2StarterCode/src/DeckTester.java
@@ -0,0 +1,23 @@
+
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+
+        String[] ranks = {"Ace", "Queen", "Three"};
+		String[] suits = {"Spades", "Hearts"};
+		int[] pointValues = {11, 10, 3};
+		Deck d = new Deck(ranks, suits, pointValues);
+		System.out.println(d);
+		System.out.println(d.deal());
+		System.out.println(d.deal());
+		System.out.println(d);
+	}
+}
